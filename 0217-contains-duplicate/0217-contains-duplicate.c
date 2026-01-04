56af9bf68c81ddcb80e6c6386183e301f5d409b8
sinjini <sinjinidatta27@gmail.com>
2026-01-04 23:34:05 +0530

    Time: 46 ms (61.95%), Space: 17.3 MB (88.16%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.c
@@ -0,0 +1,17 @@
+#include <stdbool.h>
+#include <stdlib.h>
+
+int cmp(const void *a, const void *b) {
+    return (*(int *)a - *(int *)b);
+}
+bool containsDuplicate(int* nums, int numsSize) {
+    if (numsSize <= 1)
+        return false;
+    qsort(nums, numsSize, sizeof(int), cmp);
+    for (int i = 0; i < numsSize - 1; i++) {
+        if (nums[i] == nums[i + 1])
+            return true;
+    }
+
+    return false;
+}
