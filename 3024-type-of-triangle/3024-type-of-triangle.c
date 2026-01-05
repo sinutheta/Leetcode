699c4c8cf0cf63660648890e46a50e1a38ff8784
sinjini <sinjinidatta27@gmail.com>
2026-01-05 22:22:21 +0530

    Time: 0 ms (100%), Space: 8.2 MB (68.55%) - LeetHub

---
+++ 3024-type-of-triangle/3024-type-of-triangle.c
@@ -0,0 +1,7 @@
+char* triangleType(int* nums, int numsSize) {
+  int a = nums[0], b = nums[1], c = nums[2];
+    if (a + b <= c || a + c <= b || b + c <= a) return "none";
+    if (a == b && b == c) return "equilateral";
+    if (a == b || a == c || b == c) return "isosceles";  
+     return "scalene";
+}
