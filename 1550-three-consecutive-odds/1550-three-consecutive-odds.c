4b1c84b010930fe85f7e943e55d28824340ed0fc
sinjini <sinjinidatta27@gmail.com>
2026-01-05 22:21:05 +0530

    Time: 0 ms (100%), Space: 8 MB (59.15%) - LeetHub

---
+++ 1550-three-consecutive-odds/1550-three-consecutive-odds.c
@@ -0,0 +1,9 @@
+bool threeConsecutiveOdds(int* arr, int arrSize) {
+  for(int i=0;i<arrSize-2;i++){
+        if(arr[i]%2!=0 && arr[i+1]%2!=0 && arr[i+2]%2!=0){
+            return true;
+        }
+    }
+    return false;
+  
+}
