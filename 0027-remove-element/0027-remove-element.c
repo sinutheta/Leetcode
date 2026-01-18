43dd6ebc253b5b16463d98400bb964cc7f388248
sinjini <sinjinidatta27@gmail.com>
2026-01-18 13:00:50 +0530

    Time: 0 ms (100%), Space: 9.6 MB (93.78%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.c
@@ -0,0 +1,11 @@
+int removeElement(int* nums, int numsSize, int val) {
+    if(!numsSize) return 0;
+
+    int index = 0;
+    for(int i=0; i<numsSize; i++){
+        if(nums[i] != val){
+            nums[index++] = nums[i];
+        }
+    }
+    return index;
+}
